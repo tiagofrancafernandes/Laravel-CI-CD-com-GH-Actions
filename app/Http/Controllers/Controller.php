0c31b23cb847edef302aafd959e6ac73391b6236
Tiago França <tiago@tiagofranca.com>
2023-05-20 13:44:06 -0300

    uggly/wrong file on branch master
    Changes to be committed:
    	modified:   app/Http/Controllers/Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests;
-    use ValidatesRequests;
+class Controller extends BaseController{
+    use AuthorizesRequests;    use ValidatesRequests;
 }
