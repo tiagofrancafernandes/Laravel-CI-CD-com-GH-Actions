bb8aa873305ea98a5ee5a227df585150a74a10fa
Tiago França <tiago@tiagofranca.com>
2023-05-20 17:33:28 -0300

    shh deploy fix

--- ci-cd/scripts/sail-run.sh
+++ ci-cd/scripts/sail-run.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
-    laravelsail/php82-composer:latest ${@}
+    laravelsail/php82-composer:latest $@
