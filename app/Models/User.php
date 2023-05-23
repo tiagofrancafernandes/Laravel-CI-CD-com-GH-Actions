f2fbfe2dcee5d69e443bad0f92bca22eae441574
Tiago França <tiago@tiagofranca.com>
2023-05-23 00:54:34 -0300

    added breezy package On branch filament/breezy Changes to be committed: 	modified:   app/Models/User.php 	modified:   composer.json 	modified:   composer.lock 	new file:   config/filament-breezy.php 	modified:   config/filament.php 	new file:   database/migrations/2023_05_23_034826_add_two_factor_columns_to_table.php 	new file:   resources/views/vendor/filament-breezy/.gitkeep 	new file:   resources/views/vendor/filament-breezy/components/auth-card.blade.php 	new file:   resources/views/vendor/filament-breezy/components/grid-section.blade.php 	new file:   resources/views/vendor/filament-breezy/components/sections/2fa.blade.php 	new file:   resources/views/vendor/filament-breezy/components/sections/passwords.blade.php 	new file:   resources/views/vendor/filament-breezy/components/sections/personal-info.blade.php 	new file:   resources/views/vendor/filament-breezy/components/sections/sanctum.blade.php 	new file:   resources/views/vendor/filament-breezy/filament/pages/my-profile.blade.php 	new file:   resources/views/vendor/filament-breezy/livewire/breezy-sanctum-tokens.blade.php 	new file:   resources/views/vendor/filament-breezy/login.blade.php 	new file:   resources/views/vendor/filament-breezy/register.blade.php 	new file:   resources/views/vendor/filament-breezy/reset-password.blade.php 	new file:   resources/views/vendor/filament-breezy/two-factor.blade.php 	new file:   resources/views/vendor/filament-breezy/verify.blade.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,51 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use JeffGreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $two_factor_secret
+ * @property string|null $two_factor_recovery_codes
+ * @property string|null $two_factor_confirmed_at
+ * @property-read mixed $has_confirmed_two_factor
+ * @property-read mixed $has_enabled_two_factor
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorConfirmedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
